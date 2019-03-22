75e0f02b591b71da0a5db248ae80624a1e85c610
Cixtor <cixtords@gmail.com>
2019-03-22 03:10:00 -0800

    Add API endpoint to create a contact via POST

--- main_test.go
+++ main_test.go
@@ -1,7 +1,11 @@
 package main
 
 import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
 	"os"
+	"strings"
 	"testing"
 )
 
@@ -10,11 +14,41 @@ const database = "testing.db"
 func TestInit(t *testing.T) {
 	var app Application
 	app.Init(database)
-	app.db.Close()
+	defer app.db.Close()
 
 	if _, err := os.Stat(database); os.IsNotExist(err) {
-		t.Fatalf("%s should have been created, but was not", database)
+		t.Fatalf("%s was not created", database)
 	}
 
 	os.Remove(database)
+}
+
+func TestContactCreate(t *testing.T) {
+	var app Application
+	app.Init(database)
+	defer app.db.Close()
+
+	var err error
+	var out []byte
+	var res *http.Response
+	ts := httptest.NewTLSServer(http.HandlerFunc(app.ContactCreate))
+	defer ts.Close()
+
+	client := ts.Client()
+	body := strings.NewReader(`firstname=John&lastname=Smith&phone=6045551234&address=350+W+Georgia+St%2C+Vancouver%2C+BC&email=john%40example.com`)
+	if res, err = client.Post(ts.URL, "application/x-www-form-urlencoded", body); err != nil {
+		t.Fatalf("ContactCreate; client.Post: %s", err)
+		return
+	}
+	defer res.Body.Close()
+
+	if out, err = ioutil.ReadAll(res.Body); err != nil {
+		t.Fatalf("ContactCreate; ioutil.ReadAll: %s", err)
+		return
+	}
+
+	if string(out) != "{\"ok\":true}\n" {
+		t.Fatalf("ContactCreate; failure: %s", out)
+		return
+	}
 }
--- web_contact_create.go
+++ web_contact_create.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 )
 
@@ -8,5 +10,41 @@ func init() {
 	router.POST("/contact", app.ContactCreate)
 }
 
+// ContactCreate inserts a new contact into the database.
+//
+//   > POST /contact HTTP/1.1
+//   > Content-Type: application/x-www-form-urlencoded; charset=utf-8
+//   > Host: localhost:3000
+//   > Content-Length: 115
+//   >
+//   > firstname=John&lastname=Smith&phone=6045551234&address=350+W+Georgia+St%2C+Vancouver%2C+BC&email=john%40example.com
 func (app *Application) ContactCreate(w http.ResponseWriter, r *http.Request) {
+	var err error
+	var stmt *sql.Stmt
+
+	if err = r.ParseForm(); err != nil {
+		fmt.Println("r.ParseForm", err)
+		fail(w, r, fmt.Errorf("r.ParseForm %s", err))
+		return
+	}
+
+	firstname := r.Form.Get("firstname")
+	lastname := r.Form.Get("lastname")
+	phone := r.Form.Get("phone")
+	address := r.Form.Get("address")
+	email := r.Form.Get("email")
+
+	if stmt, err = app.db.Prepare(`INSERT INTO contacts (firstname, lastname, phone, address, email) VALUES(?, ?, ?, ?, ?)`); err != nil {
+		fmt.Println("db.Prepare", err)
+		fail(w, r, fmt.Errorf("db.Prepare %s", err))
+		return
+	}
+
+	if _, err = stmt.Exec(firstname, lastname, phone, address, email); err != nil {
+		fmt.Println("stmt.Exec", err)
+		fail(w, r, fmt.Errorf("stmt.Exec %s", err))
+		return
+	}
+
+	write(w, r, Response{Ok: true})
 }
