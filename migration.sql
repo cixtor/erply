87ae19c22444501634c2c7022ac9942217559cf3
Cixtor <cixtords@gmail.com>
2019-03-22 03:23:00 -0800

    Fix database migration to insert only unique entries

--- migration.sql
+++ migration.sql
@@ -1,4 +1,4 @@
 CREATE TABLE IF NOT EXISTS contacts (id INTEGER NOT NULL PRIMARY KEY, firstname TEXT, lastname TEXT, phone TEXT, address TEXT, email TEXT);
 
-INSERT INTO contacts VALUES (1, "John", "Smith", "6045551234", "350 W Georgia St, Vancouver, BC", "john@example.com");
-INSERT INTO contacts VALUES (2, "Alice", "Smith", "6045551234", "350 W Georgia St, Vancouver, BC", "alice@example.com");
+INSERT OR IGNORE INTO contacts VALUES (1, "John", "Smith", "6045551234", "350 W Georgia St, Vancouver, BC", "john@example.com");
+INSERT OR IGNORE INTO contacts VALUES (2, "Alice", "Smith", "6045551234", "350 W Georgia St, Vancouver, BC", "alice@example.com");
